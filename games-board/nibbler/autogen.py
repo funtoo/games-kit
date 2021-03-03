493bf85c729c2af6cdb1c738c27ff5238f99c29c
Daniel Robbins <drobbins@funtoo.org>
2021-03-03 01:00:16 -0700

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      faustoo: 72f43a0a145c823e21e225a76a1d40a7eb3453eb
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: 4cf74bbe7ad8bb2aaaa89378c210eecbfbe832f4

--- games-board/nibbler/autogen.py
+++ games-board/nibbler/autogen.py
@@ -2,8 +2,8 @@
 
 
 def get_release(releases_data):
-	releases = list(filter(lambda x: x["prerelease"] is False and x["draft"] is False, releases_data))
-	return None if not releases else sorted(releases, key=lambda x: x["tag_name"]).pop()
+	releases = list(filter(lambda x: x["prerelease"] is False and x["draft"] is False and "-rc" not in x["tag_name"], releases_data))
+	return None if not releases else releases[0]
 
 
 async def generate(hub, **pkginfo):
--- games-board/nibbler/nibbler-1.6.5.ebuild
+++ games-board/nibbler/nibbler-1.6.5.ebuild
@@ -6,7 +6,7 @@ inherit desktop pax-utils xdg
 
 DESCRIPTION="Real-time chess analysis GUI"
 HOMEPAGE="https://github.com/fohristiwhirl/nibbler"
-SRC_URI="https://github.com/fohristiwhirl/nibbler/releases/download/v1.6.5-rc3/nibbler-1.6.5-rc3-linux.zip"
+SRC_URI="https://github.com/fohristiwhirl/nibbler/releases/download/v1.6.5/nibbler-1.6.5-linux.zip"
 LICENSE="GPL3"
 SLOT="0"
 
